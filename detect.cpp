78849f80a17999b5d26c7f92659271067188993e
Qian,Xiaoyu <wojiaoqiandayu@163.com>
2023-04-09 05:32:02 +0800

    reset f8cb61967

--- detect.cpp
+++ detect.cpp
@@ -54,8 +54,7 @@ int main(void){
   while(time(0)-t0 < 60){     
     for(int i = 30 ; i < 151 ; i = i+60){
       servo.setServo('0',i);
-      int t1 = time(0);
-      while(time(0) - t1 < 1){}
+      delay(0.2);
       if(i==30){
         L = u.getDistance();
       }else if(i==90){
