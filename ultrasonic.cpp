8ebbfe350d84d0b69a8979eb80a970b185e2795b
Qian,Xiaoyu <wojiaoqiandayu@163.com>
2023-04-08 09:57:36 +0800

    correct ultrasonic code

--- ultrasonic.cpp
+++ ultrasonic.cpp
@@ -17,7 +17,8 @@
 int main(void){
   pinMode(Trigger_pin,OUTPUT);
   pinMode(Echo_pin,INPUT);
-  function pulseIn(){
+
+  int pulseIn(){
     int t0 = time();
     while(digitalRead(Echo_pin) != HIGH):
         if((time() - t0) > TimeOut*0.000001):
@@ -26,16 +27,16 @@ int main(void){
     while(digitalRead(Echo_pin) == HIGH):
         if((time() - t0) > TimeOut*0.000001):
             return 0;
-    pulseTime = (time.time() - t0)*1000000;
+    int pulseTime = (time.time() - t0)*1000000;
     return pulseTime;
   }
-  distance_cm=[0,0,0,0,0];
-  int distance_cm[] = { 5, 3, 1, 4, 2 }; // 待排序的数组
+
+  int distance_cm[] = { 0, 0, 0, 0, 0 }; // 待排序的数组
 
   for(int i=0 ;i<5;i++){
-    digitalWrite(Trigger_pin,HIGH)      # make trigger_pin output 10us HIGH level 
+    digitalWrite(Trigger_pin,HIGH);      // make trigger_pin output 10us HIGH level 
     delay(0.00001);
-    digitalWrite(Trigger_pin,LOW) 
+    digitalWrite(Trigger_pin,LOW); 
     int pulseTime = pulseIn()* 340.0 / 2.0 / 10000.0;
     distance_cm[i] = pulseTime;
   }
