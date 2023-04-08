50054287b0aa538fcfaaeaae5d5243d7ee4c4b0d
Qian,Xiaoyu <wojiaoqiandayu@163.com>
2023-04-08 10:55:34 +0800

    use gettimeofday to get us

--- ultrasonic.cpp
+++ ultrasonic.cpp
@@ -2,7 +2,7 @@
 #include <wiringPi.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <sys/time.h>
 #include <algorithm>
 #define Trigger_pin 2
 #define Echo_pin 3
@@ -13,20 +13,25 @@
 //   public:
 
 // }
+long getTime(){
+  struct timeval tv1;
+  gettimeofday(&tv1, NULL);
+  return tv1.tv_sec * 1000000 + tv1.tv_usec;
+}
 int pulseIn(){
-  int t0 = time(0);
+  int t0 = getTime();
   while(digitalRead(Echo_pin) != HIGH){
-    if((time(0) - t0) > TimeOut*0.000001){
+    if((getTime() - t0) > TimeOut){
       return 0;
     }
   }
-  t0 = time(0);
+  t0 = getTime();
   while(digitalRead(Echo_pin) == HIGH){
-    if((time(0) - t0) > TimeOut*0.000001){
+    if((getTime() - t0) > TimeOut){
       return 0;
     }
   }
-  int pulseTime = (time(0) - t0)*1000000;
+  int pulseTime = (getTime() - t0);
   return pulseTime;
 }
 
