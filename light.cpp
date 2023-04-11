8b69d6341c11dacff56ec09c43cbec4263c1058c
JJinhai <962343912@qq.com>
2023-04-11 17:11:17 +0100

    use pca9685

--- light.cpp
+++ light.cpp
@@ -1,1 +1,34 @@
 //light sensor, when light is strong, car start to run
+
+#include "pca9685/pca9685.h"
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+import time
+from Motor import *
+from ADC import *
+class Light:
+    def run(self):
+        try:
+            self.adc=Adc()
+            self.PWM=Motor()
+            self.PWM.setMotorModel(0,0,0,0)
+            while True:
+                L = self.adc.recvADC(0)
+                R = self.adc.recvADC(1)
+
+                if (L > 2.99 && R > 2.99) 
+                {
+                    set.detect
+                }
+                    
+                    
+        except KeyboardInterrupt:
+           led_Car.PWM.setMotorModel(0,0,0,0) 
+
+if __name__=='__main__':
+    print ('Program is starting ... ')
+    led_Car=Light()
+    led_Car.run()
