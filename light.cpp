5b087092874db91cf7e87acc88042e479ed8a9b2
JJinhai <962343912@qq.com>
2023-04-11 17:37:32 +0100

    new

--- light.cpp
+++ light.cpp
@@ -4,14 +4,33 @@
 #include <wiringPi.h>
 #include <stdio.h>
 #include <stdlib.h>
+using namespace std;
 
 
-import time
-from Motor import *
-from ADC import *
-class Light:
-    def run(self):
-        try:
+def recvADC(self,channel):
+    if self.Index=="PCF8591":
+        data=self.recvPCF8591(channel)
+    elif self.Index=="ADS7830":
+        data=self.recvADS7830(channel)
+    return data
+
+int main(void){
+    self.adc=Adc()
+    self.PWM=Motor()
+    self.PWM.setMotorModel(0,0,0,0)
+    while True:
+        L = self.adc.recvADC(0)
+        R = self.adc.recvADC(1)
+
+        if (L > 2.99 && R > 2.99) 
+        {
+            set.detect
+        }
+                    
+                    
+        except KeyboardInterrupt:
+           led_Car.PWM.setMotorModel(0,0,0,0) 
+}
             self.adc=Adc()
             self.PWM=Motor()
             self.PWM.setMotorModel(0,0,0,0)
@@ -32,3 +51,5 @@ if __name__=='__main__':
     print ('Program is starting ... ')
     led_Car=Light()
     led_Car.run()
+
+
