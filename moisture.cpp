8ad80965141f86f79fa3b2a32d9552a219155aea
JJinhai <123516297+JJinhai@users.noreply.github.com>
2023-04-12 15:19:36 +0100

    Update moisture.cpp

--- moisture.cpp
+++ moisture.cpp
@@ -1,28 +1,28 @@
-// 接线方法：YL-69有模拟输出（AO口）和数字输出（DO口），由于树莓派的GPIO只支持数字输入，
-// 所以AO口悬空。将VCC连接物理接口2，GND引脚连接物理接口14，DO引脚连接物理接口40。
-// 接好线以后，电源灯（红灯）会亮起，调节传感器上的阈值旋钮，使得在空气中绿灯不亮。
-// 此时将传感器触角伸入水中，可以看到绿灯亮起。
+// Wiring method: The YL-69 has an analogue output (AO port) and a digital output (DO port), as the Raspberry Pi's GPIO only supports digital inputs.
+// so the AO port is left hanging. Connect VCC to physical interface 2, the GND pin to physical interface 14 and the DO pin to physical interface 40.
+// Once wired, the power light (red light) will come on, adjust the threshold knob on the sensor so that the green light does not come on in air.
+// At this point stick the sensor contacts into the water and you will see the green light come on.
 
 #include <iostream>
 #include <wiringPi.h>
 
 using namespace std;
 
-const int SOIL_MOISTURE_SENSOR_PIN = 29;  // WPI 29 管脚40，BCM编码为21
-const int WATERING_THRESHOLD_LOW = 400;  // 浇水阈值下限
-const int WATERING_THRESHOLD_HIGH = 650;  // 浇水阈值上限
+const int SOIL_MOISTURE_SENSOR_PIN = 29;  // WPI 29 pin 40, BCM code 21
+const int WATERING_THRESHOLD_LOW = 400;  // Lower watering threshold
+const int WATERING_THRESHOLD_HIGH = 650;  // Watering threshold limit
 
 int main(void) {
-    if(wiringPiSetup() == -1) {  // 初始化wiringPi库
+    if(wiringPiSetup() == -1) {  // Initialising the wiringPi library
         cout << "Failed to initialize wiringPi library." << endl;
         return 0;
     }
-    pinMode(SOIL_MOISTURE_SENSOR_PIN, INPUT);  // 设置引脚为输入模式
-    while(1) {  // 无限循环
+    pinMode(SOIL_MOISTURE_SENSOR_PIN, INPUT);  // Set pins to input mode
+    while(1) {  // Infinite loop
         if(digitalRead(SOIL_MOISTURE_SENSOR_PIN) == LOW) {
             cout << "Soil moisture result: wet" << endl;
         }else {
-          int soil_moisture_value = analogRead(SOIL_MOISTURE_SENSOR_PIN);  // 读取模拟输入值
+          int soil_moisture_value = analogRead(SOIL_MOISTURE_SENSOR_PIN);  // Reading analogue input values
           cout << "Soil moisture value: " << soil_moisture_value << endl;
           if(soil_moisture_value < WATERING_THRESHOLD_LOW) {
               cout << "Soil moisture too low, please water the plant." << endl;
@@ -31,7 +31,7 @@ int main(void) {
               cout << "Soil moisture is moderate, stop watering the plant." << endl;
           }
         }
-        delay(1000);  // 延迟1秒钟
+        delay(1000);  // Delayed by 1 second
     }
     return 0;
 }
