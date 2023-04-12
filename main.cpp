acb48fddec554fd6919c37722b1ea7169ad14181
Qian,Xiaoyu <wojiaoqiandayu@163.com>
2023-04-13 05:19:33 +0800

    combine check and guidance

--- main.cpp
+++ main.cpp
@@ -24,22 +24,41 @@ int main(void){
   Infrared inf = Infrared();
   Light l1 = Light();
 	int t0=time(0);
-  while(time(0) - t0 < 30 ){
-		float infrared_value = inf.getFrontValue();
-		cout<<"front infrared: "<<infrared_value<<"--";
-    float infrared_value_back = inf.getBackValue();
-		cout<<"back infrared: "<<infrared_value_back<<"--";
-    float light_left = l1.getLeftValue();
-    cout<<"light_left: "<<light_left<<"--";
-    float light_right = l1.getRightValue();
-    cout<<"light_right: "<<light_right<<endl;
-	}
-  // Guidance guidance1 = Guidance(fd);
-  // guidance1.go_to_garden();
-  // delay(5000);
-  // guidance1.back_home();
+  bool isRun = true;
+  int triggle = false;
 
-  
+  Guidance guidance1 = Guidance(fd);
+  bool checkEnv(){ //   std::thread loopThread = std::thread([this]() {})
+    isRun = true;
+    while(isRun){ // && time(0) - t0 < 30
+      float infrared_value = inf.getFrontValue();
+      cout<<"front infrared: "<<infrared_value<<"--";
+      float infrared_value_back = inf.getBackValue();
+      cout<<"back infrared: "<<infrared_value_back<<"--";
+      float light_left = l1.getLeftValue();
+      cout<<"light_left: "<<light_left<<"--";
+      float light_right = l1.getRightValue();
+      cout<<"light_right: "<<light_right<<endl;
+      if(infrared_value == LOW || infrared_value_back == LOW || light_left > 2000 || light_right > 2000){
+        triggle += 1;
+      }
+      if(triggle >= 3){
+        isRun = false
+        return true;
+      }
+	  }
+  }
+  guidance1.go_to_garden();
+  while(1){
+    triggle = checkEnv();
+    if(triggle){
+      guidance1.back_home();
+      triggle = false;
+      delay(10000);
+      guidance1.go_to_garden();
+    }
+  }
+
   return 0;
 }
 
