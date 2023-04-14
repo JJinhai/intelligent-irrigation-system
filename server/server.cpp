75f61c8bea87c3c08936e7c68ff3371e2aa5f4d4
weiwei <121024995+wayne-weiwei@users.noreply.github.com>
2023-04-14 17:36:13 +0100

    Update server.cpp

--- server/server.cpp
+++ server/server.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <boost/asio.hpp>
 #include <boost/bind/bind.hpp>
+#include <boost/algorithm/string.hpp>
 #include "fcgiapp.h"
 
 using boost::asio::ip::tcp;
@@ -9,10 +10,23 @@ using namespace std;
 
 const int PORT = 8080;
 
+class FastCGIClient {
+public:
+    FastCGIClient(/* ... */) {
+        // 初始化FastCGI客户端
+    }
+
+    string make_request(const string& url) {
+        // 发起HTTP/HTTPS请求并返回响应
+        return "FastCGI response";
+    }
+};
+
 class HttpServer {
 public:
     HttpServer(boost::asio::io_context& io_context)
-        : acceptor_(io_context, tcp::endpoint(tcp::v4(), PORT)) {
+        : acceptor_(io_context, tcp::endpoint(tcp::v4(), PORT)),
+          fastcgi_client_(/* ... */) {
         accept();
     }
 
@@ -24,14 +38,39 @@ private:
 
     void handle_accept(const boost::system::error_code& error) {
         if (!error) {
-            // 这里可以处理客户端连接和HTTP请求
-            // 然后你可以使用FastCGI客户端进行HTTP/HTTPS请求
+            // Read the client's request
+            boost::asio::streambuf request_buf;
+            boost::asio::read_until(*socket_, request_buf, "\r\n\r\n");
+
+            // Parse the request
+            string request(boost::asio::buffers_begin(request_buf.data()), boost::asio::buffers_end(request_buf.data()));
+            vector<string> request_lines;
+            boost::algorithm::split(request_lines, request, boost::is_any_of("\r\n"));
+
+            // Assume the first line is the request line, e.g., "GET / HTTP/1.1"
+            string request_line = request_lines[0];
+            vector<string> request_parts;
+            boost::algorithm::split(request_parts, request_line, boost::is_any_of(" "));
+
+            if (request_parts.size() >= 3) {
+                string method = request_parts[0];
+                string url = request_parts[1];
+                string http_version = request_parts[2];
+
+                // Use the FastCGI client to make an HTTP/HTTPS request
+                string response = fastcgi_client_.make_request(url);
+
+                // Send the response back to the client
+                boost::asio::write(*socket_, boost::asio::buffer(response));
+            }
         }
+
         accept();
     }
 
     tcp::acceptor acceptor_;
     std::optional<tcp::socket> socket_;
+    FastCGIClient fastcgi_client_;
 };
 
 int main() {
