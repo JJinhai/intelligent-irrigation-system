b273a63567e0609502425592687801eeda9d666d
Qian,Xiaoyu <wojiaoqiandayu@163.com>
2023-04-18 00:52:11 +0800

    motor K test class

---
+++ testMotor.cpp
@@ -0,0 +1,19 @@
+// 参考链接 https://osoyoo.com/2020/11/06/osoyoo-pie-car-v2-0-for-raspberry-pi-lesson-1-basic-framework-and-programming-gpio-pca9685-c-version/
+#include "pca9685/pca9685.h"
+#include <wiringPi.h>
+#include "motor.cpp"
+
+int main(int argc, char* argv[])
+{
+  wiringPiSetup();
+ int fd = pca9685Setup(300, 0x40, 50);
+  Motor m1 = Motor(fd);
+  int duration = std::atoi(argv[1]);
+  int direction = std::atoi(argv[2]);
+  if(direction == 1){
+    m1.MotorGo(2000,2000,2000,2000,duration);
+  }else{
+    m1.MotorGo(2000,2000,-2000,-2000,duration);
+  }
+  return 0;
+}
