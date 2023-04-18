bf4ace09b82e784a0e0c2b7c77477d62c303b0fc
Qian,Xiaoyu <qianxy5@yonyou.com>
2023-04-19 02:44:42 +0800

    lack >

--- pre_detect.cpp
+++ pre_detect.cpp
@@ -30,7 +30,7 @@ int main(void){
     cout<<"light_left: "<<light_left<<"--";
     float light_right = l1.getRightValue();
     cout<<"light_right: "<<light_right<<endl;
-    if(light_left > 3000 || light_right 3000){
+    if(light_left > 3000 || light_right > 3000){
       guidance1.go_to_garden();
       break;
     }
